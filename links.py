d0ec53d546e4e2ea667c9c6570ef40f76f96054f
MarceloGoncalvesBraga <85029465+MarceloGoncalvesBraga@users.noreply.github.com>
2022-05-19 15:43:25 -0300

    Create links.py

---
+++ links.py
@@ -0,0 +1,6 @@
+
+screens 
+https://www.figma.com/file/yn8RHFmYkQeuTyraGbt8SJ/Untitled?node-id=3%3A4
+  
+  case use
+https://docs.google.com/document/d/1Go427V_2kfKP2F0iPxJNXEZ_R30oLx67Vq2UzG1mBJI/edit#heading=h.oxparvgxq0nn
